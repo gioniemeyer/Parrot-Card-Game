0f04eeb39bbccb50a2a2c1577f87173c9cd16c14
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-04-06 19:26:12 -0300

    prompt em loop até número válido

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
+function TurnCard(NumberOfCard) {
+    const CardNumber = document.querySelector(NumberOfCard);
+}
+
+
+
 function HowManyCards() {
-    const NumberOfCards = prompt('Com quantas cartas você quer jogar?')
+    const NumberOfCards = prompt('Com quantas cartas você quer jogar?');
+    let IsValid;
+
+    if(4 < NumberOfCards < 15 && NumberOfCards % 2 === 0) {
+        isValid = true;
+    } else {
+        isValid = false;
+    }
+
+    while(isValid === false) {
+        const NumberOfCards = prompt('Com quantas cartas você quer jogar?');
+        if(4 < NumberOfCards < 15 && NumberOfCards % 2 === 0) {
+            isValid = true;
+        } else {
+            isValid = false;
+        }
+    } 
 }
 
 HowManyCards()
