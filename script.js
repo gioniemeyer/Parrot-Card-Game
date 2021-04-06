0453e7867de7865c90ccc5c299fc1b08cc98e264
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-04-06 19:36:44 -0300

    refactoring do loop do prompt

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
+let IsValid;
+let NumberOfCards;
+
 function TurnCard(NumberOfCard) {
     const CardNumber = document.querySelector(NumberOfCard);
 }
 
-
-
 function HowManyCards() {
-    const NumberOfCards = prompt('Com quantas cartas você quer jogar?');
-    let IsValid;
+
+    AskCheckNumber()
+
+    while(isValid === false) {
+        AskCheckNumber()
+    }
+
+    // fazer as cartas aparecerem com innrHTML tal como coloquei no HTML
+     
+}
+
+function AskCheckNumber() {
+    NumberOfCards = prompt('Com quantas cartas você quer jogar?');
 
     if(4 < NumberOfCards < 15 && NumberOfCards % 2 === 0) {
         isValid = true;
     } else {
         isValid = false;
     }
-
-    while(isValid === false) {
-        const NumberOfCards = prompt('Com quantas cartas você quer jogar?');
-        if(4 < NumberOfCards < 15 && NumberOfCards % 2 === 0) {
-            isValid = true;
-        } else {
-            isValid = false;
-        }
-    } 
 }
 
 HowManyCards()
