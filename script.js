9e47f140b555a6be59ad4634c41779c34ab855ae
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-04-07 23:52:16 -0300

    correção bug do prompt

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 let IsValid;
-let NumberOfCards = [];
+let NumberOfCards;
+let Cards = ["bobrossparrot", "explodyparrot", "fiestaparrot", "metalparrot", "revestitparrot", "tripletsparrot", "unicornparrot"];
 
-
-function TurnCard(NumberOfCard) {
-    const selector = NumberOfCard + " " + 'img'
-    const Card = document.querySelector(selector);
-    Card.setAttribute('src','images/bobrossparrot.gif');
-}
 
 function HowManyCards() {
-
 
     AskCheckNumber()
 
@@ -17,28 +11,32 @@ function HowManyCards() {
         AskCheckNumber()
     }
 
-    // fazer as cartas aparecerem com innrHTML tal como coloquei no HTML
-
-    for(let i = 0; i < NumberOfCards.length; i++) {
+    for(let i = 0; i < NumberOfCards / 2; i++) {
         let Board = document.querySelector('ul');
-        Board.innerHTML += `<li class="card first-pair" onclick="TurnCard(this)">
-        <img src="images/front 6.png" alt="parrot card">
+        Board.innerHTML += `<li onclick="TurnCard(this)">
+        <img class="front" src="images/front.png" alt="parrot card">
+        <img class="back hidden" src="images/${Cards[i]}.gif" alt="parrot card">
+        </li>
+        <li onclick="TurnCard(this)">
+        <img class="front" src="images/front.png" alt="parrot card">
+        <img class="back hidden" src="images/${Cards[i]}.gif" alt="parrot card">
         </li>`;
-    }
+    }     
+}
 
-            // NumberOfCards.push("teste");
-        // console.log(NumberOfCards[i]);
-     
+function TurnCard(NumberOfCard) {
+
+    alert()
 }
 
 function AskCheckNumber() {
-    NumberOfCards.length = prompt('Com quantas cartas você quer jogar?');
 
-    if(4 < NumberOfCards.length < 15 && NumberOfCards.length % 2 === 0) {
+    NumberOfCards = prompt('Com quantas cartas você quer jogar?');
+
+    if(4 < NumberOfCards && NumberOfCards < 15 && NumberOfCards % 2 === 0) {
         isValid = true;
     } else {
         isValid = false;
     }
 }
-
 HowManyCards()
